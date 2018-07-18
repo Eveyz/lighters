5bbc22187b6700f6adbc1441362689b355870905
ZNZ <nxz84@case.edu>
2018-07-18 19:21:22 -0400

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(email: "admin@lighter.com", encrypted_password: "saiop147", identity: "admin")
