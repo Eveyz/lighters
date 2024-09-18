dc9ba279e4d375498ffb4f25f091c110304a0547
Eveyz <saiop147@gmail.com>
2024-09-18 16:55:36 +0800

    update build file

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
+bun install
+
 rm -rf web/build
 cd web
+bun install
 bun run build
 cd ..
 
 rm -rf build/
 cp -r web/build/ ./build/
 
-cross-env NODE_ENV=render nodemon --exec babel-node server.js --ignore public/
+NODE_ENV=render nodemon --exec babel-node server.js --ignore public/
